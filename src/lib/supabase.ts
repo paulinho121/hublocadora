5e167c37a7f58b82fae50f1b81925fc9b482f6bd
paulinho121 <paulofernandoautomacao@gmail.com>
2026-04-14 18:09:09 -0300

    fodkofdkkkkkk

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,11 +1,6 @@
-import * as dotenv from 'dotenv';
-dotenv.config();
 import { createClient } from '@supabase/supabase-js';
 
-// @ts-ignore
-const env = typeof process !== 'undefined' ? process.env : (import.meta as any).env;
-
-const supabaseUrl = env?.VITE_SUPABASE_URL || 'https://ktcmnjtnpnocitojgrxw.supabase.co';
-const supabaseKey = env?.VITE_SUPABASE_ANON_KEY || '';
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL || 'https://ktcmnjtnpnocitojgrxw.supabase.co';
+const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY || '';
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
