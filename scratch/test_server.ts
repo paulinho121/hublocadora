7e33528621e01f7713d1c70286542f8d0450a14f
paulinho121 <paulofernandoautomacao@gmail.com>
2026-04-24 11:47:44 -0300

    kdskk

--- scratch/test_server.ts
+++
@@ -1,7 +0,0 @@
-import express from 'express';
-const app = express();
-app.get('/', (req, res) => res.send('OK'));
-app.listen(3002, () => {
-  console.log('Basic server listening on 3002');
-  process.exit(0);
-});
